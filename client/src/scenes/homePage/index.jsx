b6e71dce4466af040b6b504a12c8d8dcbfc058e0
LindaEng <90lineng@gmail.com>
2023-02-17 19:57:45 -0500

    nav bar completed

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,8 +1,12 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import Navbar from '../navBar'
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Box>
+      <Navbar />
+    </Box>
   )
 }
 
