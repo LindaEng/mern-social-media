729ac0bc3ef1ed39c776122f6a118e9c16ecec96
LindaEng <90lineng@gmail.com>
2023-02-19 12:51:39 -0500

    register schema added to loginPage

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -17,6 +17,15 @@ import Dropzone from "react-dropzone" //can drag and drop images
 import FlexBetween from "components/FlexBetween"
 
 //yup validation schema
+const registerSchema = yup.object().shape({
+    firstName: yup.string().required("required"),
+    lastName: yup.string().required("required"),
+    email: yup.string().email("invalid email").required("required"),
+    password: yup.string().required("required"),
+    location: yup.string().required("required"),
+    occupation: yup.string().required("required"),
+    picture: yup.string().required("required"),
+})
 
 const Form = () => {
   return (
